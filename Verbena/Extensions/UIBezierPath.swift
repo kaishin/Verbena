68cb460ea3d07a441aa819dce0e18fd1f3057d8b
Reda Lemeden <reda@thoughtbot.com>
2015-06-08 08:11:05 -0700

    Make UIBezier extension internal

--- Verbena/Extensions/UIBezierPath.swift
+++ Verbena/Extensions/UIBezierPath.swift
@@ -4,7 +4,7 @@ extension UIBezierPath {
   /// Fills a path with a given color.
   ///
   /// :param: color The color to use for filling the path.
-  public func fill(#color: UIColor) {
+  func fill(#color: UIColor) {
     color.setFill()
     self.fill()
     UIColor.clearColor().setFill()
