50c55ba6154a33939df10dd655ade9b92794fa75
Reda Lemeden <reda@thoughtbot.com>
2015-06-14 03:06:17 +0200

    Add code for drawing logo in the demo

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -11,8 +11,20 @@ class ViewController: UIViewController {
       var ovalPath = UIBezierPath(ovalInRect: CGRect(origin: CGPoint(x: 0, y: 0), size: size))
       UIColor(hue: 0.289, saturation: 0.485, brightness: 0.792, alpha: 1.0).setFill()
       ovalPath.fill()
-    }
 
+      var letterPath = UIBezierPath()
+      let startPoint = CGPoint(x: 48, y: 60)
+      letterPath.moveToPoint(startPoint)
+      letterPath.addLineToPoint(CGPoint(x: 76, y: 65))
+      letterPath.addLineToPoint(CGPoint(x: 100, y: 110))
+      letterPath.addLineToPoint(CGPoint(x: 122, y: 65))
+      letterPath.addLineToPoint(CGPoint(x: 150, y: 60))
+      letterPath.addLineToPoint(CGPoint(x: 100, y: 158))
+      letterPath.addLineToPoint(startPoint)
+      letterPath.closePath()
+      UIColor.whiteColor().setFill()
+      letterPath.fill()
+    }
   }
 }
 
--- Demo/DemoMac/ViewController.swift
+++ Demo/DemoMac/ViewController.swift
@@ -10,6 +10,19 @@ class ViewController: NSViewController {
       var ovalPath = NSBezierPath(ovalInRect: CGRect(origin: CGPoint(x: 0, y: 0), size: size))
       NSColor(hue: 0.289, saturation: 0.485, brightness: 0.792, alpha: 1.0).setFill()
       ovalPath.fill()
+
+      var letterPath = NSBezierPath()
+      let startPoint = CGPoint(x: 48, y: 140)
+      letterPath.moveToPoint(startPoint)
+      letterPath.lineToPoint(CGPoint(x: 76, y: 135))
+      letterPath.lineToPoint(CGPoint(x: 100, y: 90))
+      letterPath.lineToPoint(CGPoint(x: 122, y: 135))
+      letterPath.lineToPoint(CGPoint(x: 150, y: 140))
+      letterPath.lineToPoint(CGPoint(x: 100, y: 42))
+      letterPath.lineToPoint(startPoint)
+      letterPath.closePath()
+      NSColor.whiteColor().setFill()
+      letterPath.fill()
     }
   }
 }
