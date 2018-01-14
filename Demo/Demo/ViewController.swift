49112a9f8feaab960be4a7a1ccc7a98603e35039
Reda Lemeden <git@kaishin.haz.email>
2018-01-14 16:57:52 +0100

    Update demos to Swift 4

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -7,22 +7,22 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    imageView.image = UIImage.render(CGSize(width: 200, height: 200), transparency: true) { size in
-      let ovalPath = UIBezierPath(ovalInRect: CGRect(origin: CGPoint(x: 0, y: 0), size: size))
+    imageView.image = UIImage.render(imageView.bounds.size, transparency: true) { size in
+      let ovalPath = UIBezierPath(ovalIn: CGRect(origin: CGPoint(x: 0, y: 0), size: size))
       UIColor(hue: 0.289, saturation: 0.485, brightness: 0.792, alpha: 1.0).setFill()
       ovalPath.fill()
 
       let letterPath = UIBezierPath()
       let startPoint = CGPoint(x: 48, y: 60)
-      letterPath.moveToPoint(startPoint)
-      letterPath.addLineToPoint(CGPoint(x: 76, y: 65))
-      letterPath.addLineToPoint(CGPoint(x: 100, y: 110))
-      letterPath.addLineToPoint(CGPoint(x: 122, y: 65))
-      letterPath.addLineToPoint(CGPoint(x: 150, y: 60))
-      letterPath.addLineToPoint(CGPoint(x: 100, y: 158))
-      letterPath.addLineToPoint(startPoint)
-      letterPath.closePath()
-      UIColor.whiteColor().setFill()
+      letterPath.move(to: startPoint)
+      letterPath.addLine(to: CGPoint(x: 76, y: 65))
+      letterPath.addLine(to: CGPoint(x: 100, y: 110))
+      letterPath.addLine(to: CGPoint(x: 122, y: 65))
+      letterPath.addLine(to: CGPoint(x: 150, y: 60))
+      letterPath.addLine(to: CGPoint(x: 100, y: 158))
+      letterPath.addLine(to: startPoint)
+      letterPath.close()
+      UIColor.white.setFill()
       letterPath.fill()
     }
   }
